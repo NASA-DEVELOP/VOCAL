63d88f2915501ed32b6c884e9aa2c4b7ca471074
Syntaf <gmercer015@gmail.com>
2015-06-15 15:06:46 -0400

    move child window to seperate class

--- gui/CALIPSO_Visualization_Tool.py
+++ gui/CALIPSO_Visualization_Tool.py
@@ -15,6 +15,7 @@ from gui.plot_depolar_ratio import drawDepolar
 from gui.plot_uniform_alt_lidar_dev import drawBackscattered
 from tools import createToolTip, ToggleableButton, NavigationToolbar2CALIPSO, \
     ToolbarToggleableButton
+from toolswindow import toolsWindow
 
 #### PROGRAM CONSTANTS ####
 HEIGHT          = 665
@@ -48,41 +49,17 @@ class Calipso(object):
         
         ######################################### CREATE CHILD WINDOW #########################################
         
-        self.__child = Toplevel()
-        self.__child.title("Tools")
-        self.__child.resizable(width=FALSE, height=FALSE)
-        baseChildPane = PanedWindow(self.__child)
-        baseChildPane.pack(fill=BOTH, expand = 1)
-        sectionedChildPane = PanedWindow(self.__child, orient=VERTICAL)
-        baseChildPane.add(sectionedChildPane)
-        self.__child.protocol("WM_DELETE_WINDOW", Calipso.ignore)
-                
-        upperPane = PanedWindow(sectionedChildPane, orient=HORIZONTAL, width=5)
-        sectionedChildPane.add(upperPane)
-        lowerPane = PanedWindow(sectionedChildPane)
-        sectionedChildPane.add(lowerPane)
+        self.__child = toolsWindow(self, r)
         
-        self.__upperButtonFrame = Frame(upperPane)                                  # upper button frame holding text buttons
-        self.__upperButtonFrame.pack()                                              
-            
-        self.__lowerButtonFrame = Frame(lowerPane)                                  # lower button frame for tools
-        self.__lowerButtonFrame.config(highlightthickness=1)                        # create a small border around the frame
-        self.__lowerButtonFrame.config(highlightbackground="grey")
-        self.__lowerButtonFrame.pack()
-        
-        self.__coordinateFrame = Frame(lowerPane, width=50, height=50)
-        self.__coordinateFrame.config(highlightthickness=1)                        # create a small border around the frame
-        self.__coordinateFrame.config(highlightbackground="grey")
-        self.__coordinateFrame.pack(side=BOTTOM, fill=BOTH)
-        
-######################################### INIT CANVAS #########################################
+        ######################################### INIT CANVAS #########################################
 
-                # the main canvas we will be drawing our data to
+        # the main canvas we will be drawing our data to
         self.__drawplotCanvas = FigureCanvasTkAgg(self.__Parentfig, master=self.__drawplotFrame)    
         # create tool bar and polygonDrawer     
-        self.__toolbar = NavigationToolbar2CALIPSO(self.__drawplotCanvas, self.__coordinateFrame)
-        self.__polygonList = PolygonList(self.__drawplotCanvas)
-        
+        self.toolbar = NavigationToolbar2CALIPSO(self.__drawplotCanvas, self.__child.coordinateFrame)
+        # list of object drawn to the screen
+        self.polygonList = PolygonList(self.__drawplotCanvas)
+        # show the frame
         self.__drawplotCanvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
         self.__drawplotFrame.pack()
         
@@ -140,16 +117,16 @@ class Calipso(object):
     # parameter: plotType = int value(0-2) associated with desired plotType
     def selPlot(self, plotType):
         if (plotType) == Constants.BASE_PLOT:
-            self.__polygonList.setPlot(Constants.BASE_PLOT)
+            self.polygonList.setPlot(Constants.BASE_PLOT)
         elif (plotType.get()) == Constants.BACKSCATTERED:
             try:
                 self.__Parentfig.clear()
                 self.__fig = self.__Parentfig.add_subplot(1,1,1)
                 drawBackscattered(self.__file, self.__fig, self.__Parentfig)
                 self.__drawplotCanvas.show()
-                self.__polygonList.setPlot(Constants.BACKSCATTERED)
+                self.polygonList.setPlot(Constants.BACKSCATTERED)
                 self.__drawplotCanvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=0)
-                self.__toolbar.update()
+                self.toolbar.update()
                 self.__drawplotCanvas._tkcanvas.pack(side=LEFT, fill=BOTH, expand=0)
             except IOError:
                 filewin = Toplevel(self.__root)
@@ -161,10 +138,10 @@ class Calipso(object):
                 self.__Parentfig.clear()
                 self.__fig = self.__Parentfig.add_subplot(1, 1, 1)
                 drawDepolar(self.__file, self.__fig, self.__Parentfig)
-                self.__polygonList.setPlot(Constants.DEPOLARIZED)
+                self.polygonList.setPlot(Constants.DEPOLARIZED)
                 self.__drawplotCanvas.show()
                 self.__drawplotCanvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=0)
-                self.__toolbar.update()
+                self.toolbar.update()
                 self.__drawplotCanvas._tkcanvas.pack(side=LEFT, fill=BOTH, expand=0)
             except IOError:
                 filewin = Toplevel(self.__root)
@@ -181,8 +158,8 @@ class Calipso(object):
     # Reload the initial image
     def reset(self):
         #reset radio-buttons
-        self.__polygonList.reset()
-        self.__toolbar.home()
+        self.polygonList.reset()
+        self.toolbar.home()
         
     def createTopScreenGUI(self):
         #File Dialog box, - shows the selected __file
@@ -194,7 +171,7 @@ class Calipso(object):
         btnBrowse.grid(row=1, column=3)
         
     def notifySaveDB(self):
-        self.__polygonList.save()
+        self.polygonList.save()
         filewin = Toplevel(self.__root)
         filewin.title("Notice")
         filewin.geometry('%dx%d+%d+%d' % (100, 75, self.x + self.x*2 - 60, self.y + self.y/2 + 160))
@@ -206,105 +183,6 @@ class Calipso(object):
         
     def dbOpenDialog(self):
         pass
-        
-    def createChildWindowGUI(self):
-        ###################################Upper Frame##############################################
-        
-        btnReset = Button(self.__upperButtonFrame, text = "Reset", width = 10, command=self.reset)
-        btnReset.grid(row=0, column=0, padx=10, pady=5)
-        
-        #Plot Type Selection - Radio-button determining how to plot the __file
-        menubtnPlotSelection = Menubutton(self.__upperButtonFrame, text="Plot Type", relief=RAISED, width = 10)
-        menubtnPlotSelection.grid(row=0, column=1, padx=10, pady=5)
-        menubtnPlotSelection.menu = Menu(menubtnPlotSelection, tearoff=0)
-        menubtnPlotSelection["menu"]=menubtnPlotSelection.menu
-        
-        plotType = IntVar()
-        menubtnPlotSelection.menu.add_radiobutton(label="Backscattered", variable=plotType, value=Constants.BACKSCATTERED, command=lambda: self.selPlot(plotType))
-        menubtnPlotSelection.menu.add_radiobutton(label="Depolarization Ratio", variable=plotType, value=Constants.DEPOLARIZED, command=lambda: self.selPlot(plotType))
-        menubtnPlotSelection.menu.add_radiobutton(label="VFM Plot", variable=plotType, value=Constants.VFM, command=lambda: self.selPlot(plotType))
-        
-        ###################################Lower Frame##############################################
-        
-        lblSpace1 = Label(self.__lowerButtonFrame, width=1)     # create space between frame outline
-        lblSpace1.grid(row=0, column=0)
-            
-        lblSpace2 = Label(self.__lowerButtonFrame, width=1)
-        lblSpace2.grid(row=0, column=5)
-        
-        # magnify icon
-        self.magnifydrawIMG = ImageTk.PhotoImage(file="ico/magnify.png")
-        self.__zoomButton = ToolbarToggleableButton(self.__root, self.__lowerButtonFrame, lambda : self.__toolbar.zoom(True), image=self.magnifydrawIMG, width=30, height=30)
-        self.__zoomButton.latch(cursor="tcross")
-        self.__zoomButton.grid(row=0, column=2, padx=2, pady=5)
-        createToolTip(self.__zoomButton, "Zoom to rect")
-        
-        # plot move cursor icon
-        self.plotcursorIMG = ImageTk.PhotoImage(file="ico/plotcursor.png")
-        self.__plotCursorButton = ToolbarToggleableButton(self.__root, self.__lowerButtonFrame, lambda : self.__toolbar.pan(True), image=self.plotcursorIMG, width=30, height=30)
-        self.__plotCursorButton.latch(cursor="hand1")
-        self.__plotCursorButton.grid(row=0, column=1, padx=2, pady=5)
-        createToolTip(self.__plotCursorButton, "Move about plot")
-        
-        # plot undo icon
-        self.undoIMG = ImageTk.PhotoImage(file="ico/back.png")
-        self.__undoButton = Button(self.__lowerButtonFrame, image=self.undoIMG, width=30, height=30, command=lambda : self.__toolbar.back(True))
-        self.__undoButton.grid(row=0, column=3, padx=2, pady=5)
-        createToolTip(self.__undoButton, "Previous View")
-        
-        # plot redo icon
-        self.redoIMG = ImageTk.PhotoImage(file="ico/forward.png")
-        self.__redoButton = Button(self.__lowerButtonFrame, image=self.redoIMG, width=30, height=30, command=lambda : self.__toolbar.forward(True))
-        self.__redoButton.grid(row=0, column=4, padx=2, pady=5)
-        createToolTip(self.__redoButton, "Next View")
-
-        # drawBackscattered rectangle shape
-        self.polygonIMG = ImageTk.PhotoImage(file="ico/polygon.png")
-        self.__polygonButton = ToggleableButton(self.__root, self.__lowerButtonFrame, image=self.polygonIMG, width=30, height=30)
-        self.__polygonButton.latch(key="<Button-1>", command=self.__polygonList.anchorRectangle, cursor="tcross")
-        self.__polygonButton.latch(key="<B1-Motion>", command=self.__polygonList.rubberBand, cursor="tcross")
-        self.__polygonButton.latch(key="<ButtonRelease-1>", command=self.__polygonList.fillRectangle, cursor="tcross")
-        self.__polygonButton.grid(row=1, column=1, padx=2, pady=5)
-        createToolTip(self.__polygonButton, "Draw Rect")
-        
-        # free form shape creation
-        self.freedrawIMG = ImageTk.PhotoImage(file="ico/freedraw.png")
-        self.__freedrawButton = ToggleableButton(self.__root, self.__lowerButtonFrame, image=self.freedrawIMG, width=30, height=30)
-        self.__freedrawButton.latch(key="<Button-1>", command=self.__polygonList.plotPoint, cursor="tcross")
-        self.__freedrawButton.grid(row=1, column=3, padx= 2, pady=5)
-        createToolTip(self.__freedrawButton, "Free Draw")
-        
-        # move polygon and rectangles around
-        self.dragIMG = ImageTk.PhotoImage(file="ico/cursorhand.png")
-        self.__dragButton = ToggleableButton(self.__root, self.__lowerButtonFrame, image=self.dragIMG, width=30, height=30)
-        self.__dragButton.latch(key="<Button-2>", command=self.__polygonList.toggleDrag, cursor="hand1")
-        self.__dragButton.grid(row=1, column=2, padx=2, pady=5)
-        createToolTip(self.__dragButton, "Drag")
-        
-        # erase polygon drawings
-        self.eraseIMG = ImageTk.PhotoImage(file="ico/eraser.png")
-        self.__eraseButton = ToggleableButton(self.__root, self.__lowerButtonFrame, image=self.eraseIMG, width=30, height=30)
-        self.__eraseButton.latch(key="<Button-1>", command=self.__polygonList.delete, cursor="X_cursor")
-        self.__eraseButton.grid(row=1, column=4, padx=2, pady=5)
-        createToolTip(self.__eraseButton, "Erase polygon")
-
-        self.paintIMG = ImageTk.PhotoImage(file="ico/paint.png")
-        self.__paintButton = ToggleableButton(self.__root, self.__lowerButtonFrame, image=self.paintIMG, width=30, height=30)
-        self.__paintButton.latch(key="<Button-1>", command=self.__polygonList.paint, cursor="")
-        self.__paintButton.grid(row=2, column=2, padx=2, pady=5)
-        createToolTip(self.__paintButton, "Paint")
-
-        self.outlineIMG = ImageTk.PhotoImage(file="ico/focus.png")
-        self.__outlineButton = Button(self.__lowerButtonFrame, image=self.outlineIMG, width=30, height=30, command=lambda: self.__polygonList.outline())
-        self.__outlineButton.grid(row=2, column=1, padx=2, pady=5)
-        createToolTip(self.__outlineButton, "Focus")
-        
-        self.plotIMG = ImageTk.PhotoImage(file="ico/hide.png")
-        self.__plotButton = Button(self.__lowerButtonFrame, image=self.plotIMG, width=30, height=30, command=lambda: self.__polygonList.hide())
-#       self.__plotButton.latch(key="<Button-1>", command=self.__polygonList.hide, cursor="")
-        self.__plotButton.grid(row=2, column=3, padx=2, pady=5)
-        createToolTip(self.__plotButton, "Hide polygons")
-
 
     def importFile(self):
         ftypes = [('CALIPSO Data files', '*.hdf'), ('All files', '*')]
@@ -314,7 +192,7 @@ class Calipso(object):
             self.__file = fl
             Segments = self.__file.rpartition('/')
             self.__lblFileDialog.config(width = 50, bg = white, relief = SUNKEN, justify = LEFT, text = Segments[2])
-            self.__polygonList.setHDF(self.__file)
+            self.polygonList.setHDF(self.__file)
         return ''
     
     def exportImage(self):
@@ -341,7 +219,7 @@ class Calipso(object):
     # Setup the body of the GUI, initialize the default image (CALIPSO_A_Train.jpg)
     def setupMainScreen(self):
         self.createTopScreenGUI()
-        self.createChildWindowGUI()
+        self.__child.setupToolBarButtons()
         self.selPlot(Constants.BASE_PLOT)
         
 
