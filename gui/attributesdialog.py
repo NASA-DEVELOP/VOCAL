c13e777e558e09aa0e05c260c6b83b0963b88918
stapleCamel <ntqian@email.wm.edu>
2015-06-18 10:56:29 -0400

    Skeleton attributes window

--- gui/attributesdialog.py
+++ gui/attributesdialog.py
@@ -5,7 +5,9 @@ Created on Jun 15, 2015
 
 '''
 
-from Tkinter import Toplevel
+from Tkconstants import TOP, X, BOTH, DISABLED
+from Tkinter import Toplevel, Frame, StringVar, Label, Text, Button
+
 
 class attributesDialog(Toplevel):
     '''
@@ -15,5 +17,61 @@ class attributesDialog(Toplevel):
         '''
         Initialize root tkinter window and master GUI window
         '''
-        self.__root = root
-        self.__master= master
+        Toplevel.__init__(self, root, width=200, height=200)
+        self.__master = master
+        self.title("Edit Attributes")
+        
+        self.container = Frame(self)
+        self.container.pack(side=TOP, fill=BOTH, expand=True) 
+            
+        self.createTopFrame()
+        self.createBottomFrame()
+        
+    def createTopFrame(self):
+        self.topFrame = Frame(self.container)                       
+        self.topFrame.pack(side=TOP, fill=X, expand=False)
+        
+        attributeString = StringVar()
+        attributeString.set("Attributes:")
+        attributeLabel = Label(self.topFrame, textvariable=attributeString)
+        attributeLabel.grid(row=0, column=0)
+        
+        selectedString = StringVar()
+        selectedString.set("Selected:")
+        selectedLabel = Label(self.topFrame, textvariable=selectedString)
+        selectedLabel.grid(row=0, column=3)
+        
+        attributeText = Text(self.topFrame)
+        attributeText.grid(row=1, column=0)
+        attributeText.config(state=DISABLED)
+        
+        moveButton = Button(self.topFrame, width=3, height=2)
+        moveButton.grid(row=1, column=1)
+        
+        removeButton = Button(self.topFrame, width=3, height=2)
+        removeButton.grid(row=1, column=2)
+        
+        selectedText = Text(self.topFrame)
+        selectedText.grid(row=1, column=3)
+        selectedText.config(state=DISABLED)
+        
+    def createBottomFrame(self):
+        self.bottomFrame = Frame(self.container)                       
+        self.bottomFrame.pack(side=TOP, fill=X, expand=False)
+        
+        noteString = StringVar()
+        noteString.set("Notes:")
+        noteLabel = Label(self.bottomFrame, textvariable=noteString)
+        noteLabel.grid(row=0, column=0)
+        
+        noteText = Text(self.bottomFrame)
+        noteText.grid(row=1, column=0)
+        
+        acceptButton = Button(self.bottomFrame)
+        acceptButton.grid(row=2, column=0)
+        
+        cancelButton = Button(self.bottomFrame)
+        cancelButton.grid(row=2, column=1)
+        
+        closeButton = Button(self.bottomFrame)
+        closeButton.grid(row=2, column=2)
