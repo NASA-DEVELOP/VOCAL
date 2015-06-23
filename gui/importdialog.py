db574dec0ab35730b332d7e375bc7fda44900c35
Syntaf <gmercer015@gmail.com>
2015-06-23 11:38:32 -0400

    importDialog docs for search function

--- gui/importdialog.py
+++ gui/importdialog.py
@@ -5,7 +5,7 @@ Created on Jun 15, 2015
 
 '''
 
-import collections, string
+import collections
 from Tkinter import Toplevel, Entry, Button, BOTH, Frame, \
     Label, BOTTOM, TOP, X, RIDGE
 
@@ -62,42 +62,36 @@ class dbDialog(Toplevel):
         self.filterButton.grid(row=0, column=3, padx=5, pady=10)
         
     def refineSearch(self, event):
-        lst = list()
-        if event.char.isalnum(): self.__searchString += event.char
-        print "char: ", repr(event.char), "searchString ", self.__searchString
-        if self.e.get() != '':
-            print "not empty...."
-            if event.char == '':
-                print "popping stack..."
-                self.__searchString = self.__searchString[:-1]
-                if self.__stack : 
-                    del self.tree.info[:]
+        '''
+        Function to dynamically narrow the results of a search while the
+        user types into the search bar. Checks if the character is 
+        alpha numeric , and if so queries the database for the combined
+        string. A backend stack keeps track of past searches, when the
+        user enters the backspace code a previous instance is popped
+        and reloaded.
+        '''
+        if event.char.isalnum(): self.__searchString += event.char                  # if character is a letter / number, add to the searchstring
+        if self.e.get() != '':                                                      # if the entry box is NOT empty
+            if event.char == '':                                                    # if a backspace code is entered
+                self.__searchString = self.__searchString[:-1]                      # remove one letter from search string
+                if self.__stack :                                                   # pop previous search and display to tree
                     self.tree.info = self.__stack.pop()
                     self.tree.update()
-            elif event.char.isalnum():
-                print "searching...", [s[4] for s in self.tree.info if self.__searchString in s[4]]
-                for obj in self.session.query(dbPolygon).filter(or_(
-                        dbPolygon.tag.contains(self.__searchString), 
+            elif event.char.isalnum():                                              # else if the character is alphanumeric
+                lst = list()
+                for obj in self.session.query(dbPolygon).filter(or_(                # query the database for if self.__searchstring is contained in 
+                        dbPolygon.tag.contains(self.__searchString),                # fields listed
                         dbPolygon.attributes.contains(self.__searchString),
                         dbPolygon.notes.contains(self.__searchString))):
-                    lst.append(                                                          # user see's this list
+                    lst.append(                                                     # append any objects that were returned by the query
                         (obj.tag, obj.plot, obj.time_, obj.hdf, obj.attributes[1:-1], obj.notes)
                     )
-                self.__stack.append(self.tree.info)
-                self.tree.info = lst
+                self.__stack.append(self.tree.info)                                 # push the new list onto our stack
+                self.tree.info = lst                                                # set the new display list and update
                 self.tree.update()
         else:
             self.__searchString = ""
             self.__displayAll()
-            
-        """
-        for obj in self.session.query(dbPolygon).filter(or_( \
-                        dbPolygon.tag.in_([s[0] for s in self.tree.info if self.__searchString in s[0]]), \
-                        dbPolygon.attributes.in_([s[4] for s in self.tree.info if self.__searchString in s[4]]))):
-        """
-            
- 
-        #print "tree: ",self.tree.list
         
     def createBottomFrame(self):
         '''
@@ -130,8 +124,6 @@ class dbDialog(Toplevel):
         items = self.tree.tree.selection()
         for tag in items:
             tag = self.tree.tree.item(tag, option="values")
-            # the tag represents the selected item, but must be converted to an index
-            #print self.__itList[idx]
             names = [x.tag for x in self.__itList]
             self.__master.getPolygonList().readPlot(
                 readFromString=str(self.__itList[names.index(tag[0])]))
@@ -141,6 +133,9 @@ class dbDialog(Toplevel):
         pass
     
     def __displayAll(self):
+        '''
+        Helper function to simply display all objects in the database
+        '''
         lst = list()
         self.__offset = 0
         if self.tree.info : self.__stack.append(self.tree.info)
