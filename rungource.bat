23ddce871c18c6991913dcf01227497b2a339d7a
Syntaf <gmercer015@gmail.com>
2015-06-24 19:42:23 -0400

    added linux gource file

--- rungource.bat
+++ rungource.bat
@@ -1,1 +1,1 @@
-gource.exe -s 3 --hide filenames,mouse --highlight-users --highlight-colour FF90FF --multi-sampling --dir-colour C0C0C0 --viewport 1920x1080 --title "CALIPSO" --output-framerate 60
+gource.exe -s 1 --hide filenames,mouse --highlight-users --highlight-colour FF90FF --multi-sampling --dir-colour C0C0C0 --viewport 1920x1080 --title "CALIPSO" --output-framerate 60
