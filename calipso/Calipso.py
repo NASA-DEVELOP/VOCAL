bd665031a78d43e04f1f4bdaef752ade24a56372
stapleCamel <ntqian@email.wm.edu>
2015-06-30 09:11:01 -0400

    Fixed relative file path name

--- calipso/Calipso.py
+++ calipso/Calipso.py
@@ -319,7 +319,7 @@ class Calipso(object):
 def main():
     import logging.config
     logging.basicConfig(filename=constants.FILE_NAME)
-    logging.config.fileConfig('C:/Users/nqian/git/Calipso_Visualization/calipso/log/logging.ini', disable_existing_loggers=False)
+    logging.config.fileConfig('log/logging.ini', disable_existing_loggers=False)
     logging.info("Starting CALIPSO program")
     rt = Tk()
     program = Calipso(rt)       # Create main GUI window
