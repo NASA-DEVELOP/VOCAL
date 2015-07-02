5d29b9b3e98ee9764ade937df4aa8ce14b4df142
stapleCamel <ntqian@email.wm.edu>
2015-07-02 11:22:54 -0400

    Created math module

--- calipso/polygon/drawer.py
+++ calipso/polygon/drawer.py
@@ -9,7 +9,10 @@ from numpy import empty_like, dot, array
 
 import constants
 from log import logger
-    
+from polygon.LinearAlgebra import tupleToNpArray, getIntersection, \
+    npArrayToTuple, isIntersecting
+
+
 class PolygonDrawer(object):
     '''
     Displays the polygon objects onto the canvas by supplying draw methods.
@@ -497,58 +500,3 @@ class PolygonDrawer(object):
         logger.info("Toggling drag")
         PolygonDrawer.dragToggle = not PolygonDrawer.dragToggle
                             
-def perpendicular(a):
-    '''
-    Returns a numpy array that's orthogonal to the param
-    '''
-    b = empty_like(a)
-    b[0] = -a[1]
-    b[1] = a[0]
-    return b
-
-def getIntersection(a1, a2, b1, b2):
-    '''
-    Retrieves the point of intersection of two lines given two points
-    on each line
-    :param a1, a2: Two points on the first line
-    :param b1, b1: Two points on the second line
-    '''
-    da = a2 - a1
-    db = b2 - b1
-    dp = a1 - b1
-    dap = perpendicular(da)
-    denom = dot(dap, db)
-    num = dot(dap, dp)
-    return (num /denom.astype(float))*db + b1
-
-def isIntersecting(a1, a2, b1, b2):
-    '''
-    Determines if two line segments are intersecting by checking if the point of intersection
-    exists on the line segments
-    :param a1, a2: The endpoints of the first line segment
-    :param b1, b2: The endpoints of the second line segment
-    '''
-    point = getIntersection(a1, a2, b1, b2)
-    if ((point[0] < max(min(a1[0], a2[0]), min(b1[0], b2[0]))) or
-        (point[0] > min(max(a1[0], a2[0]), max(b1[0], b2[0])))):
-        return False
-    else:
-        return True
-    
-def tupleToNpArray(pair):
-    '''
-    Converts a tuple to a numpy array
-    
-    :param pair: Tuple
-    '''
-    return array([pair[0], pair[1]])
-    
-def npArrayToTuple(array):
-    '''
-    Converts a numpy array to a tuple
-    
-    :param array: Numpy array
-    '''
-    x = array[0]
-    y = array[1]
-    return (x, y)
