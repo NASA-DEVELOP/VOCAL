d7b188377807ab86abc751516d60a02635eec5d3
Syntaf <gmercer015@gmail.com>
2015-07-03 14:27:01 -0400

    refactored import dialog

--- calipso/importdialog.py
+++ calipso/importdialog.py
@@ -7,181 +7,196 @@
 
 from Tkinter import Toplevel, Entry, Button, BOTH, Frame, \
     Label, BOTTOM, TOP, X, RIDGE
-import collections
-import tkMessageBox
-
 from sqlalchemy import or_
-
-import constants
 from db import db, dbPolygon
 from tools.tools import center
 from tools.treelistbox import TreeListBox
 from log import logger
+import collections
+import tkMessageBox
+import constants
 
-#import TkTreectrl as treectrl
-#import db
-class dbDialog(Toplevel):
-    '''
+
+class ImportDialog(Toplevel):
+    """
     Dialog window which prompts user for a selection of objects to import as well as
     showing a customizable list for displaying the data
-    '''
+
+    :param: root: root Tk widget, often Tk()
+    :param: master: the main window, for access of polygonList
+    """
+
     def __init__(self, root, master):
-        '''
-        :param: root: root Tk widget, often Tk()
-        :param: master: the main window, for access of polygonList
-        '''
-        logger.info("Instantiating dbDialog")
+        logger.info('Instantiating dbDialog')
         Toplevel.__init__(self, root)
+
         self.protocol('WM_DELETE_WINDOW')
-                
         self.session = db.getSession()
-        self.__itList = list()
+        self.__internal_list = list()
         self.__stack = collections.deque(maxlen=15)
-        self.__searchString = ""
-        self.__master = master        
-        self.title("Import from existing database")
-        center(self, (constants.IMPORTWIDTH,constants.IMPORTHEIGH)) # simple function to center window and set size
-        
-        self.container = Frame(self)                                # create center frame, for use of splitting window horizontally later
-        self.container.pack(side=TOP, fill=BOTH, expand=True)       # place
-        
-        self.createTopFrame()                                       # create the top frame and pack buttons / etc. on it
-        self.createBottomFrame()                                    # create the bottom frame and pack
-        
-    def createTopFrame(self):
-        '''
+        self.__search_string = ''
+        self.__master = master
+        self.title('Import from existing database')
+        self.tree = None
+        self.e = None
+        self.top_frame = None
+        self.bottom_frame = None
+        self.bottom_button_frame = None
+        self.separator = None
+
+        center(self, (constants.IMPORTWIDTH, constants.IMPORTHEIGH))  # simple function to center window and set size
+
+        self.container = Frame(self)  # create center frame,
+        self.container.pack(side=TOP, fill=BOTH, expand=True)  # place
+
+        self.create_top_frame()  # create the top frame and pack buttons / etc. on it
+        self.create_bottom_frame()  # create the bottom frame and pack
+
+    def create_top_frame(self):
+        """
         Initialize the upper frame of the window in charge of buttons
-        '''
-        logger.info("Creating top frame")
-        self.topFrame = Frame(self.container)                       # create top frame
-        self.topFrame.pack(side=TOP, fill=X, expand=False)
-        
-        self.label = Label(self.topFrame, text="Search ")           # search label 
-        self.e = Entry(self.topFrame)                               # input box for searching specific attributes
-        self.e.bind("<KeyRelease>", self.refineSearch)
-        self.label.grid(row=0, column=0, padx=5, pady=10)
+        """
+        logger.info('Creating top frame')
+        self.top_frame = Frame(self.container)  # create top frame
+        self.top_frame.pack(side=TOP, fill=X, expand=False)
+
+        label = Label(self.top_frame, text='Search ')  # search label
+        self.e = Entry(self.top_frame)  # input box for searching specific attributes
+        self.e.bind('<KeyRelease>', self.refine_search)
+        label.grid(row=0, column=0, padx=5, pady=10)
         self.e.grid(row=0, column=1, padx=5, pady=10)
-            
-        lblSpace1 = Label(self.topFrame, width=20)     # create space between frame outline
-        lblSpace1.grid(row=0, column=2)
-        self.topFrame.columnconfigure(2, weight=1)
-            
+
+        spacer = Label(self.top_frame, width=20)  # create space between frame outline
+        spacer.grid(row=0, column=2)
+        self.top_frame.columnconfigure(2, weight=1)
+
         # custom command for filtering objects by properties
-        self.deleteButton = Button(self.topFrame, text="Delete", command=self.deleteDb,
-                                   width=10)
-        self.deleteButton.grid(row=0, column=3, padx=15)
-        
-    def refineSearch(self, event):
-        '''
+        delete_button = Button(self.top_frame, text='Delete', command=self.delete_from_db,
+                               width=10)
+        delete_button.grid(row=0, column=3, padx=15)
+
+    def refine_search(self, event):
+        """
         Function to dynamically narrow the results of a search while the
-        user types into the search bar. Checks if the character is 
+        user types into the search bar. Checks if the character is
         alpha numeric , and if so queries the database for the combined
         string. A backend stack keeps track of past searches, when the
         user enters the backspace code a previous instance is popped
         and reloaded.
         :param event: search box events
-        '''
-        if event.char.isalnum(): self.__searchString += event.char                  # if character is a letter / number, add to the searchstring
-        if self.e.get() != '':                                                      # if the entry box is NOT empty
-            if event.char == '':                                                    # if a backspace code is entered
-                self.__searchString = self.__searchString[:-1]                      # remove one letter from search string
-                if self.__stack :                                                   # pop previous search and display to tree
+        """
+        # If character is a letter or number, add to search_string
+        if event.char.isalnum():
+            self.__search_string += event.char
+        # If the entry box is NOT empty
+        if self.e.get() != '':
+            # But If a backspace is entered
+            if event.char == '':
+                # Remove one letter from search string and pop stack
+                self.__search_string = self.__search_string[:-1]
+                if self.__stack:
                     self.tree.info = self.__stack.pop()
                     self.tree.update()
-            elif event.char.isalnum():                                              # else if the character is alphanumeric
+            # Else if the character is alphanumeric
+            elif event.char.isalnum():
+                # Temporary variable to create new list
                 lst = list()
-                for obj in self.session.query(dbPolygon).filter(or_(                # query the database for if self.__searchstring is contained in 
-                        dbPolygon.tag.contains(self.__searchString),                # fields listed
-                        dbPolygon.attributes.contains(self.__searchString),
-                        dbPolygon.notes.contains(self.__searchString))):
-                    lst.append(                                                     # append any objects that were returned by the query
-                        (obj.tag, obj.plot, obj.time_, obj.hdf, obj.attributes[1:-1], obj.notes)
-                    )
-                self.__stack.append(self.tree.info)                                 # push the new list onto our stack
-                self.tree.info = lst                                                # set the new display list and update
+                # For all objects in the database
+                for obj in self.session.query(dbPolygon).filter(
+                        or_(  # query the database for if search_string is contained in
+                              dbPolygon.tag.contains(self.__search_string),
+                              dbPolygon.attributes.contains(self.__search_string),
+                              dbPolygon.notes.contains(self.__search_string))):
+                    lst.append(  # append any objects that were returned by the query
+                                 (obj.tag, obj.plot, obj.time_, obj.hdf, obj.attributes[1:-1], obj.notes))
+                # Push new query onto the stack and set display to list
+                self.__stack.append(self.tree.info)
+                self.tree.info = lst
                 self.tree.update()
         else:
-            self.__searchString = ""
-            self.__displayAll()
-        logger.info("Displaying refined search")
-        
-    def createBottomFrame(self):
-        '''
+            self.__search_string = ''
+            self.__display_all()
+        logger.info('Displaying refined search')
+
+    def create_bottom_frame(self):
+        """
         Create and display database in listbox, also add lower button frame for import
         button
-        '''
-        logger.info("Creating bottom frame")
-        self.bottomFrame = Frame(self.container)                                            # create bottom frame
-        self.bottomFrame.pack(side=BOTTOM, fill=BOTH, expand=True)          
-        self.separator = Frame(self.bottomFrame, relief=RIDGE, height=2, bg="gray")         # tiny separator splitting the top and bottom frame
+        """
+        logger.info('Creating bottom frame')
+        self.bottom_frame = Frame(self.container)  # create bottom frame
+        self.bottom_frame.pack(side=BOTTOM, fill=BOTH, expand=True)
+        self.separator = Frame(self.bottom_frame, relief=RIDGE, height=2,
+                               bg='gray')  # tiny separator splitting the top and bottom frame
         self.separator.pack(side=TOP, fill=X, expand=False)
-        self.bottomButtonFrame = Frame(self.bottomFrame)                                    # bottom frame for import button
-        self.bottomButtonFrame.pack(side=BOTTOM, fill=X, expand=False)
-        
-        self.tree = TreeListBox(self.bottomFrame,
-            ['name', 'plot', 'date', 'file', 'attributes', 'notes'])
-        
+        self.bottom_button_frame = Frame(self.bottom_frame)  # bottom frame for import button
+        self.bottom_button_frame.pack(side=BOTTOM, fill=X, expand=False)
+
+        self.tree = TreeListBox(self.bottom_frame,
+                                ['name', 'plot', 'date', 'file', 'attributes', 'notes'])
+
         for obj in self.session.query(dbPolygon).all():
-            self.__itList.append(obj)                                                       # insert JSON obj representation into internal list
-        
-        self.__displayAll()
-           
-        self.button = Button(self.bottomButtonFrame, text="Import", width=30,
-                             command=self.importSelection)
-        self.button.pack(side=BOTTOM, pady=10)
-    
-    def importSelection(self):
-        '''
-        Import selected objects from libox into program
-        '''
-        logger.info("Importing selection")
+            self.__internal_list.append(obj)  # insert JSON obj representation into internal list
+
+        self.__display_all()
+
+        button = Button(self.bottom_button_frame, text='Import', width=30,
+                        command=self.import_selection)
+        button.pack(side=BOTTOM, pady=10)
+
+    def import_selection(self):
+        """
+        Import selected objects from internal_list into program
+        """
+        logger.info('Importing selection')
         items = self.tree.tree.selection()
+        # For all selected items in window
         for tag in items:
-            logger.info("Encoding selection to JSON")
-            tag = self.tree.tree.item(tag, option="values")
-            names = [x.tag for x in self.__itList]
-            logger.info("Forward JSON to be read")
+            # Find those items in internal list and import them
+            logger.info('Encoding selection to JSON')
+            tag = self.tree.tree.item(tag, option='values')
+            names = [x.tag for x in self.__internal_list]
+            logger.info('Forward JSON to be read')
             self.__master.get_polygon_list().readPlot(
-                readFromString=str(self.__itList[names.index(tag[0])]))
-        logger.info("Done")
+                readFromString=str(self.__internal_list[names.index(tag[0])]))
+        logger.info('Done')
         self.free()
-            
-    def deleteDb(self):
-        '''
+
+    def delete_from_db(self):
+        """
         Delete selected objects from database
-        '''
+        """
         items = self.tree.tree.selection()
-        if tkMessageBox.askyesno("Delete?", "Really delete these items?", parent=self):
+        if tkMessageBox.askyesno('Delete?', 'Really delete these items?', parent=self):
             for tag in items:
-                tag = self.tree.tree.item(tag, option="values")
-                idx = self.__itList[[x.tag for x in self.__itList].index(tag[0])].id
-                logger.info("Notfiy db of deletion for selection")
+                tag = self.tree.tree.item(tag, option='values')
+                idx = self.__internal_list[[x.tag for x in self.__internal_list].index(tag[0])].id
+                logger.info('Notifying db of deletion from selection')
                 db.deleteItem(idx)
-            self.__displayAll()
-        logger.info("Done")
-    
-    def __displayAll(self):
-        '''
+            self.__display_all()
+        logger.info('Done')
+
+    def __display_all(self):
+        """
         Helper function to simply display all objects in the database
-        '''
-        logger.info("Display entries")
+        """
+        logger.info('Displaying entries')
         lst = list()
-        self.__offset = 0
-        if self.tree.info : self.__stack.append(self.tree.info)
+        # Push previous display to stack
+        if self.tree.info:
+            self.__stack.append(self.tree.info)
         for obj in self.session.query(dbPolygon).all():
-            lst.append(                                                          # user see's this list
-                (obj.tag, obj.plot, obj.time_, obj.hdf, obj.attributes[1:-1], obj.notes)
-            )
-            
+            lst.append(  # user see's this list
+                         (obj.tag, obj.plot, obj.time_, obj.hdf, obj.attributes[1:-1], obj.notes))
+
         self.tree.info = lst
         self.tree.update()
-        
+
     def free(self):
-        '''
+        """
         Free window
-        '''
-        logger.info("Closing window")
+        """
+        logger.info('Closing window')
         self.session.commit()
         self.session.close()
         self.destroy()
-        
--- calipso/log.py
+++ calipso/log.py
@@ -1,11 +1,11 @@
 import logging.config
 import sys
 
-def uncaughtException(exctype, value, tb):
+def uncaught_exception(exctype, value, tb):
     logger.exception("Uncaught exception: {0}".format(str(value)))
     sys.__excepthook__(exctype, value, tb)
     
-sys.excepthook = uncaughtException
-#logging.config.fileConfig(r'/home/gdev/Github/vocal/calipso/log/logging.ini', disable_existing_loggers=False)
+sys.excepthook = uncaught_exception
+# logging.config.fileConfig(r'/home/gdev/Github/vocal/calipso/log/logging.ini', disable_existing_loggers=False)
 logger = logging.getLogger('VOCAL')    
 logging.config.fileConfig('log/logging.ini', disable_existing_loggers=False)
