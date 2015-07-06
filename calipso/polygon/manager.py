dd06f5a478c597d1529f7e1756cf9efdb11a293b
Syntaf <gmercer015@gmail.com>
2015-07-06 10:37:39 -0400

    Implemented ShapeManager rubberband , fill_rectangle and anchor_rectangle base impl

--- calipso/polygon/manager.py
+++ calipso/polygon/manager.py
@@ -5,6 +5,10 @@
 #    @author: Grant Mercer
 ######################################
 
+import constants
+from polygon.shape import Shape
+from log import logger
+
 class ShapeManager(object):
     """
     Manages all shapes present on the screen, writes to database on
@@ -15,6 +19,13 @@ class ShapeManager(object):
     hide_toggle = True
 
     def __init__(self, canvas, master):
+        self.__canvas = canvas
+        self.__master = master
+        self.__current_plot = constants.BASE_PLOT
+        logger.info('Defining initial shape manager')
+        self.__shape_dict = {constants.BACKSCATTERED_STR: [Shape()],
+                             constants.DEPOLARIZED_STR: [Shape()],
+                             constants.VFM_STR: [Shape()]}
         pass
 
     def on_token_buttonpress(self, event):
@@ -30,7 +41,14 @@ class ShapeManager(object):
         pass
 
     def anchor_rectangle(self, event):
-        print "Anchoring: ", event.xdata, event.ydata
+        """
+        Informs the correct shape list's blank object to plot a corner of a rectangle.
+
+        :param event: A backend passed ``matplotlib.backend_bases.MouseEvent`` object
+        """
+        if self.__current_plot == constants.BASE_PLOT:
+            return
+        logger.info('Anchoring %d, %d' % (event.xdata, event.ydata))
 
     def get_count(self):
         pass
@@ -42,17 +60,33 @@ class ShapeManager(object):
         pass
 
     def rubberband(self, event):
-        print "Rubberbanding: ", event.xdata, event.ydata
-        pass
+        """
+        Uses a blank shape to draw 'helper rectangles' that outline the final shape of the
+        object
+
+        :param event: A backend passes ``matplotlib.backend_bases.MouseEvent`` object
+        """
+        if self.__current_plot == constants.BASE_PLOT:
+            return
+        if event.button == 1 and event.xdata and event.ydata:
+            logger.info('Rubberbanding: %d, %d' % (event.xdata, event.ydata))
 
     def fill_rectangle(self, event):
-        print "Filling: ", event.xdata, event.ydata
-        pass
+        """
+        Informs the correct shape list's blank object to draw a rectangle to the screen
+        using the provided cooridnates
+
+        :param event: A backend passed ``matplotlib.backend_bases.MouseEvent`` object
+        """
+        if self.__current_plot == constants.BASE_PLOT:
+            return
+        logger.info('Filling: %d, %d' % (event.xdata, event.ydata))
 
     def set_hdf(self, hdf_filename):
         pass
 
     def draw(self, figure, plot):
+        self.__current_plot = plot
         pass
 
     def generate_tag(self, index=-1):
