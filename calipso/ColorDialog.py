57c70159cc25b0f940308caaaff967378cd71c46
stapleCamel <ntqian@email.wm.edu>
2015-07-07 13:26:18 -0400

    Color window

---
+++ calipso/ColorDialog.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 7, 2015
+
+@author: nqian
+'''
+
+class CologDialog(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
