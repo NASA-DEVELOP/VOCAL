99b8db9b7dd86222a70eed7b48bd8506a9d9ef08
stapleCamel <ntqian@email.wm.edu>
2015-07-16 11:06:31 -0400

    Experimenting with cx_freeze

---
+++ calipso/setup.py
@@ -0,0 +1,31 @@
+#===============================================================================
+# 
+# Created on Jul 16, 2015
+# 
+# @author: nqian
+# 
+#===============================================================================
+
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+    
+options = {
+           'build_exe':{
+                        'packages': ['tkinter',
+                                     'matplotlib',
+                                     
+                                     ]
+                        }
+           }
+    
+executables = [Executable('Calipso.py', base=base)]
+
+setup(name='test',
+      version='0.1',
+      description='a test',
+      executables=executables
+    )
