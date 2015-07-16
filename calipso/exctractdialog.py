94205695f8d836bef6ae7407c622dd9d3ac31057
Syntaf <gmercer015@gmail.com>
2015-07-16 13:19:48 -0400

    Holding off on full histogram implementation

--- calipso/exctractdialog.py
+++ calipso/exctractdialog.py
@@ -27,16 +27,17 @@ class ExtractDialog(Toplevel):
 
     def __init__(self, root, shape, filename, x_range, y_range):
         """
-        Instantiates attributes
+        Instantiates attributess
         
         :param: root: root Tk widget
         :param: shape: The shape that bounds the data
         :param: filename: The name of the file on display 
         """
-        logger.info('Opening ExtractDialog')
         Toplevel.__init__(self, root)
+        self.wm_withdraw()
         self.geometry('+%d+%d' % (root.winfo_rootx(), root.winfo_rooty()))
         self.transient(root)
+        logger.info('Opening ExtractDialog')
 
         self.__root = root
         self.shape = shape
@@ -54,33 +55,39 @@ class ExtractDialog(Toplevel):
         self.canvas.get_tk_widget().grid(row=0)
         self.title('Data Subplot')
 
+        logger.info('Reading shape data')
+        self.create_subplot()
+        self.update()
+        self.deiconify()
+
+        logger.info('Creating histogram')
         x = self.__root.winfo_rootx() + self.winfo_x()*2
         y = self.__root.winfo_rooty()
-        """
         self.histogram_window = Toplevel(self)
         self.histogram_window.geometry('+%d+%d' % (x, y))
         self.histogram_window.protocol('WM_DELETE_WINDOW', ExtractDialog.ignore)
         self.histogram_window.transient(self.__root)
-        self.histogram_window.update()
+        self.histogram_window.withdraw()
 
-        self.hist_fig = Figure(figsize=(8, 5))
+        self.hist_fig = Figure(figsize=(5, 2))
         self.hist_ax = self.hist_fig.add_subplot(1, 1, 1)
         self.hist_ax.set_xlabel('Time')
         self.hist_ax.set_ylabel('Altitude (km)')
 
-        self.hist_canvas = FigureCanvasTkAgg(self.hist_fig, master=self)
+        self.hist_canvas = FigureCanvasTkAgg(self.hist_fig, master=self.histogram_window)
         self.hist_canvas.show()
         self.hist_canvas.get_tk_widget().grid(row=0)
         self.histogram_window.title('Histogram')
-        """
-        logger.info('Reading shape data')
-        self.read_shape_data()
+
+        #self.create_histogram()
+        self.histogram_window.update()
+        self.histogram_window.deiconify()
 
     @staticmethod
     def ignore():
         pass
         
-    def read_shape_data(self):
+    def create_subplot(self):
         """
         Read coordinate data from the shape to determine the new bounds of the subplot.
         Shape bounds are taken with simple ``max()`` and ``min()`` functions, however
@@ -92,7 +99,6 @@ class ExtractDialog(Toplevel):
 
         cords = self.shape.get_coordinates()
         time_cords, altitude_cords = zip(*cords)
-        print self.x_range
         x1 = self.x_range[0]
         x2 = self.x_range[1]
         h1 = min(altitude_cords)
@@ -149,3 +155,49 @@ class ExtractDialog(Toplevel):
             
             self.ax.get_xaxis().set_major_formatter(mpl.dates.DateFormatter('%H:%M:%S'))
 
+    def create_histogram(self):
+        cords = self.shape.get_coordinates()
+        time_cords, altitude_cords = zip(*cords)
+        x1 = self.x_range[0]
+        x2 = self.x_range[1]
+        h1 = min(altitude_cords)
+        h2 = max(altitude_cords)
+        nz = 500
+        colormap = 'dat/calipso-backscatter.cmap'
+
+        # TODO Show correct plot when depolarized starts working
+        plot = self.shape.get_plot()
+        with HDF(self.filename) as product:
+            time = product['Profile_UTC_Time'][x1:x2, 0]
+            height = product['metadata']['Lidar_Data_Altitudes']
+            dataset = product['Total_Attenuated_Backscatter_532'][x1:x2]
+            n_time = np.array([mpl.dates.date2num(ccplot.utils.calipso_time2dt(t)) for t in time])
+
+            min_time = min(time_cords)
+            max_time = max(time_cords)
+
+            logger.info("Applying search algorithm to determine shape bounds")
+            x1 = int(interpolation_search(n_time, min_time, TIME_VARIANCE))
+            x2 = int(interpolation_search(n_time, max_time, TIME_VARIANCE))
+
+            logger.info("Setting bounds for new subplot")
+            time = time[x1:x2]
+            dataset = dataset[x1:x2]
+            time = np.array([ccplot.utils.calipso_time2dt(t) for t in time])
+
+
+            dataset = np.ma.masked_equal(dataset, -9999)
+            _x = np.arange(x1, x2, dtype=np.float32)
+            _z, null = np.meshgrid(height, _x)
+            data = interp2d_12(
+                dataset[::],
+                _x.astype(np.float32),
+                _z.astype(np.float32),
+                x1, x2, x2 - x1,
+                h2, h1, nz)
+
+            self.hist_ax.hist(data.tolist(), facecolor='green', alpha=0.75)
+            self.hist_ax.set_xlabel('Value')
+            self.hist_ax.set_ylabel('Count')
+            self.hist_ax.set_xlim([0, 0.009])
+            self.hist_ax.set_ylim([0, 1000])
