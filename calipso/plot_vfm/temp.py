f1046d9a388a879c0e9730438622bfd08bac4c90
stapleCamel <ntqian@email.wm.edu>
2015-08-04 09:34:39 -0400

    Deleted dataextractor

--- calipso/plot_vfm/temp.py
+++ calipso/plot_vfm/temp.py
@@ -1,58 +1,34 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import Tkinter as tk
+import ttk
 
+class TestButton(object):
 
-class Borg:
-    __shared_state = {}
+    def __init__(self, root, text):
+        self.root = root
+        self.text = text
+        self.toggle = True
+        self.style = ttk.Style()
+        self.button = ttk.Button(self.root, text=self.text, command=lambda: self.push(), style='SunkableButton.TButton')
+        #self.button.pack(fill=tk.BOTH, expand=True)
 
-    def __init__(self):
-        self.__dict__ = self.__shared_state
-        self.state = 'Init'
+    def push(self):
+        self.toggle = not self.toggle
+        if self.toggle:
+            self.button.state(['!pressed'])
+            self.style.configure('SunkableButton.TButton', relief=tk.RAISED)
+        else:
+            self.button.state(['pressed'])
+            self.style.configure('SunkableButton.TButton', relief=tk.SUNKEN)
+            
+    def grid(self, row, column):
+        self.button.grid(row=row, column=column)
 
-    def __str__(self):
-        return self.state
-
-
-class YourBorg(Borg):
-    pass
-
-def out_of_scope():
-    rm3 = Borg()
-    rm3.state = 'Test'
-
-if __name__ == '__main__':
-    rm1 = Borg()
-    rm2 = Borg()
-
-    rm1.state = 'Idle'
-    rm2.state = 'Running'
-
-    out_of_scope()
-
-    print('rm1: {0}'.format(rm1))
-    print('rm2: {0}'.format(rm2))
-
-    rm2.state = 'Zombie'
-
-    print('rm1: {0}'.format(rm1))
-    print('rm2: {0}'.format(rm2))
-
-    print('rm1 id: {0}'.format(id(rm1)))
-    print('rm2 id: {0}'.format(id(rm2)))
-
-    rm3 = YourBorg()
-
-    print('rm1: {0}'.format(rm1))
-    print('rm2: {0}'.format(rm2))
-    print('rm3: {0}'.format(rm3))
-
-### OUTPUT ###
-# rm1: Running
-# rm2: Running
-# rm1: Zombie
-# rm2: Zombie
-# rm1 id: 140732837899224
-# rm2 id: 140732837899296
-# rm1: Init
-# rm2: Init
-# rm3: Init
+root = tk.Tk()
+root.geometry('200x100')
+test_button = TestButton(root, 'Test')
+test_button.grid(0, 0)
+another_button = TestButton(root, 'Another Button')
+another_button.grid(0, 1)
+other_button = TestButton(root, 'other')
+other_button.grid(1, 0)
+root.mainloop()
